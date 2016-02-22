dc41b3b8e4a5b26416fd6348168268cdec7fecbd
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-22 18:53:50 +0000

    Added social links toggle function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
-(function() {
-})();
+(() => {
+  const lb = document.querySelector('.lb');
+  const socialLinks = document.querySelector('.js-social');
+  const lbimg = lb.getAttribute('src');
+
+  const toggleSocial = (e) => {
+    let target = e.target;
+
+    if(target.hasAttribute('data-img-ref')) {
+      let ref = target.getAttribute('data-img-ref');
+      lb.setAttribute('src', `/img/${ref}-logo.svg`);
+    }
+  };
+
+  const revertLb = () => {
+   lb.setAttribute('src', lbimg);
+  };
+
+  socialLinks.addEventListener('mouseover', toggleSocial);
+  socialLinks.addEventListener('mouseout', revertLb);
+
+}());
