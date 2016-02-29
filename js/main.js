834e9bda1d473bf709b34c49552ba46f4e68ca7f
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-29 19:14:03 +0000

    Twitter button

--- js/main.js
+++ js/main.js
@@ -18,8 +18,4 @@
 
   socialLinks.addEventListener('mouseover', toggleSocial);
   socialLinks.addEventListener('mouseout', revertLb);
-
 })();
-
-// twitter follow
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
