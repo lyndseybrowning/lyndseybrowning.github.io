17a4f621f1abd3457eb3adb1db8d0be7611b5ebc
Lyndsey Browning <lbrowning86@gmail.com>
2019-10-29 19:35:23 +0000

    Move app title and subtitle into config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import GlobalStyle, { theme } from "./GlobalStyle";
 import PostList from "./PostList";
 import Post from "./Post";
 import PageNotFound from "./PageNotFound";
+import { APP_TITLE, APP_SUBTITLE } from "../scripts/config";
 
 const App = () => (
     <BrowserRouter>
@@ -12,8 +13,8 @@ const App = () => (
             <div className="o-wrapper">
                 <GlobalStyle />
                 <header>
-                    <h1>Lyndsey Browning</h1>
-                    <h2>Front End Developer</h2>
+                    <h1>{APP_TITLE}</h1>
+                    <h2>{APP_SUBTITLE}</h2>
                 </header>
                 <Switch>
                     <Route path="/" exact component={PostList} />
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,3 +1,5 @@
 module.exports = {
+    APP_TITLE: "Lyndsey Browning",
+    APP_SUBTITLE: "Senior Front End Developer",
     PAGE_SIZE: 5,
 };
