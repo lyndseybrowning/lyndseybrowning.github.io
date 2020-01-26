cc77bb6a9159dfe733b859730e9f8334b5f8ec40
Lyndsey Browning <lbrowning86@gmail.com>
2020-01-26 16:46:10 +0000

    Add test for Tags component

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { TagListStyled, TagStyled } from "./Tags.styled";
 
 const Tags = ({ keywords }) => (
-    <TagListStyled aria-label="keywords">
+    <TagListStyled aria-label="keywords" data-testid="tags">
         {keywords.map(keyword => (
             <TagStyled key={keyword}>{keyword}</TagStyled>
         ))}
     </TagListStyled>
 );
+
+Tags.propTypes = {
+    keywords: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Tags;
---
+++ src/components/tags/__tests__/Tags.test.jsx
@@ -0,0 +1,21 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Tags from "../";
+
+describe("Tags component", () => {
+    describe("when given keywords", () => {
+        it("should render a list of keywords", async () => {
+            const keywords = ["one", "two"];
+
+            const { getAllByRole } = render(<Tags keywords={keywords} />);
+            const listItems = getAllByRole("listitem");
+
+            expect(listItems.length).toBe(keywords.length);
+            expect(listItems[0]).toHaveTextContent("one");
+            expect(listItems[1]).toHaveTextContent("two");
+        });
+    });
+});
